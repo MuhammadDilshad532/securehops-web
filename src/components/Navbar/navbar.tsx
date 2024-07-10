f27cd792302fde15cdde30dcb07a174415cfbcda
Muhammad Dilshad <dilshadahamad532280@gmail.com>
2024-07-10 11:30:28 +0500

    add the map in nave bar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -2,6 +2,14 @@
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 
+const menuItems = [
+    { name: 'Home', href: '#' },
+    { name: 'About', href: '#' },
+    { name: 'Services', href: '#' },
+    { name: 'Blogs', href: '#' },
+    { name: 'Contact Us', href: '#' }
+];
+
 const Navbar = () => {
     const [isClick, setIsClick] = useState(false);
 
@@ -11,26 +19,26 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='bg-black'>
+            <nav className=''>
                 <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
                     <div className="flex items-center justify-between h-16">
                         <div className="flex items-center">
                             <div className="flex-shrink-0">
-                                <a href="/" className='text-white'>Logo</a>
+                                <a href="/" className=''>Securehops</a>
                             </div>
                         </div>
                         <div className='hidden md:block'>
                             <div className='ml-4 flex items-center space-x-4'>
-                                <a href="#" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Home</a>
-                                <a href="#" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>About</a>
-                                <a href="#" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Services</a>
-                                <a href="#" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Blogs</a>
-                                <a href="#" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Contact Us</a>
+                                {menuItems.map((item, index) => (
+                                    <a key={index} href={item.href} className='p-2'>
+                                        {item.name}
+                                    </a>
+                                ))}
                             </div>
                         </div>
                         <div className='md:hidden flex items-center'>
                             <button
-                                className='inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+                                className='inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
                                 onClick={toggleNavbar}
                             >
                                 {isClick ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
@@ -40,13 +48,13 @@ const Navbar = () => {
                 </div>
             </nav>
             {isClick && (
-                <div className='md:hidden bg-black'>
+                <div className='md:hidden'>
                     <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                        <a href="#" className='text-white hover:bg-white hover:text-black block rounded-lg p-2'>Home</a>
-                        <a href="#" className='text-white hover:bg-white hover:text-black block rounded-lg p-2'>About</a>
-                        <a href="#" className='text-white hover:bg-white hover:text-black block rounded-lg p-2'>Services</a>
-                        <a href="#" className='text-white hover:bg-white hover:text-black block rounded-lg p-2'>Blogs</a>
-                        <a href="#" className='text-white hover:bg-white hover:text-black block rounded-lg p-2'>Contact Us</a>
+                        {menuItems.map((item, index) => (
+                            <a key={index} href={item.href} className='block border-b-2 p-2'>
+                                {item.name}
+                            </a>
+                        ))}
                     </div>
                 </div>
             )}
